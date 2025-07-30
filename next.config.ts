ce8b72c6630ede967e35c2934fe519b3cf9447ea
SecondNiceee <col1596321@gmail.com>
2025-07-30 06:10:55 +0300

    Добавил payload

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+import { withPayload } from "@payloadcms/next/withPayload";
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
@@ -68,3 +69,4 @@ const nextConfig: NextConfig = {
 }
 
 export default nextConfig
+)withPayload(
